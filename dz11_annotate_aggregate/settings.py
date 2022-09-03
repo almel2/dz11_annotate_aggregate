902f458fdb0a982a9e809749322ce469f54d5586
Aleksandr <Al.Melnikov2@gmail.com>
2022-09-03 14:18:33 +0300

    added postgres

--- dz11_annotate_aggregate/settings.py
+++ dz11_annotate_aggregate/settings.py
@@ -85,12 +85,25 @@ WSGI_APPLICATION = 'dz11_annotate_aggregate.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'dz11_annotate_aggregate',
+        'USER': 'admin',
+        'PASSWORD': 'zxcvbnm8',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
+
 
 # CACHES = {
 #     'default': {
